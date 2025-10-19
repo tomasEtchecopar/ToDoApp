2ce679ac55a387ce753fdba02b2e8a221d5402f2
tomasEtchecopar <tomasEtchecopar2203@gmail.com>
2025-10-19 00:04:12 -0300

    task table done

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,10 +1,11 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { ItemForm } from './to-do/item-form/item-form';
+import { ItemList } from "./to-do/item-list/item-list";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, ItemForm],
+  imports: [RouterOutlet, ItemForm, ItemList],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
--- src/app/to-do/item-list/item-list.ts
+++ src/app/to-do/item-list/item-list.ts
@@ -1,11 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { ItemStore } from '../item-store';
+import { FormsModule, ReactiveFormsModule, ɵInternalFormsSharedModule} from "@angular/forms";
 
 @Component({
   selector: 'app-item-list',
-  imports: [],
+  imports: [ɵInternalFormsSharedModule, ReactiveFormsModule, FormsModule],
   templateUrl: './item-list.html',
   styleUrl: './item-list.css'
 })
 export class ItemList {
 
+  private readonly itemStore = inject(ItemStore);
+  protected readonly items = this.itemStore.getItems();
 }
