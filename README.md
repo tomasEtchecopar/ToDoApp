b4b75547ee5aca8e791ad6734889e6d820fdb011
tomasEtchecopar <tomasEtchecopar2203@gmail.com>
2025-10-19 11:59:44 -0300

    commit

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # ToDoApp
 To-list app made in angular
+
+/CONECTAR ANGULAR A SPRING(CRUD)
+/lograr persistencia en archivo
--- frontend/src/app/to-do/item-list/item-list.ts
+++ frontend/src/app/to-do/item-list/item-list.ts
@@ -1,6 +1,8 @@
 import { Component, inject } from '@angular/core';
 import { ItemStore } from '../item-store';
 import { FormsModule, ReactiveFormsModule, ɵInternalFormsSharedModule} from "@angular/forms";
+import { ItemService } from '../item-service';
+import { Item } from '../item';
 
 @Component({
   selector: 'app-item-list',
@@ -9,7 +11,25 @@ import { FormsModule, ReactiveFormsModule, ɵInternalFormsSharedModule} from "@a
   styleUrl: './item-list.css'
 })
 export class ItemList {
-
-  private readonly itemStore = inject(ItemStore);
+ 
+ /*  private readonly itemStore = inject(ItemStore);
   protected readonly items = this.itemStore.getItems();
-}
+ */ 
+   items: Item[] = [];
+
+  
+  constructor(private svc: ItemService){}
+
+  ngOnInit(){
+    this.load();
+  }
+
+  load(){
+    this.svc.getAll().subscribe(itms => this.items = itms);
+  }
+
+  onToggle(item: Item){
+    const updated = {...item, done: !item.done};
+    this.svc.update(updated).subscribe(() => this.load());
+  }
+ }
